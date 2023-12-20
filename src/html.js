c57195bab302ada6c558ef84aac5a77fdd7d6e4f
Kevin Villaruz <68512476+kevinvillaruz@users.noreply.github.com>
2023-12-20 15:25:13 +0800

    Update html.js

--- src/html.js
+++ src/html.js
@@ -6,11 +6,11 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
 
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-56RVBPGG');</script>
+})(window,document,'script','dataLayer','GTM-56RVBPGG');
 
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
