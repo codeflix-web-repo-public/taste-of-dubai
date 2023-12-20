15df1219a2b0d4e15bf61f5fc66fdb5cad160310
Kevin Villaruz <68512476+kevinvillaruz@users.noreply.github.com>
2023-12-20 15:20:59 +0800

    Update html.js

--- src/html.js
+++ src/html.js
@@ -5,6 +5,13 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-56RVBPGG');</script>
+
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
@@ -17,6 +24,10 @@ export default function HTML(props) {
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
+
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-56RVBPGG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
         {props.preBodyComponents}
         <div
           key={`body`}
