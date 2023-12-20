88beda593d579f938d6f256012d5ab9a30fd253e
Kevin Villaruz <68512476+kevinvillaruz@users.noreply.github.com>
2023-12-20 10:28:27 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
 			resolve: `gatsby-plugin-google-analytics`,
 			  options: {
 				// The property ID; the tracking code won't be generated without it
-				trackingId: "G-WFH7PYNGJ6",
+				trackingId: 'G-WFH7PYNGJ6',
 				// Defines where to place the tracking script - `true` in the head and `false` in the body
 				head: true,
 				// Setting this parameter is optional
@@ -49,7 +49,7 @@ module.exports = {
 				// Any additional optional fields
 				sampleRate: 5,
 				siteSpeedSampleRate: 10,
-				cookieDomain: "tasteofdubaifestival.com",
+				cookieDomain: 'tasteofdubaifestival.com',
 				// defaults to false
 				enableWebVitalsTracking: true,
 			  },
