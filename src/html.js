a9f9647cd2c0ae5f1f9ca72134999b6c86ab1517
Kevin Villaruz <68512476+kevinvillaruz@users.noreply.github.com>
2023-12-20 11:33:49 +0800

    Update html.js

--- src/html.js
+++ src/html.js
@@ -5,15 +5,6 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
       <head>
-      <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WFH7PYNGJ6"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-WFH7PYNGJ6');
-</script>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
