04b3ff9696924478b4a617f915d0ab73b3f75c86
Kevin Villaruz <68512476+kevinvillaruz@users.noreply.github.com>
2024-01-17 15:38:10 +0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ require("dotenv").config({
 module.exports = {
 	siteMetadata: {
 		title: `Taste of Dubai Festival`,
-		description: `Dubai’s Ultimate Celebration of Food, Drink and Music returns! March 1 - 3 2024`,
+		description: `Dubai’s Ultimate Celebration of Food, Drink and Music returns! 23-25 February 2024`,
 		author: `Dewynters`,
 		siteUrl: process.env.GATSBY_FRONTEND_URL
 	},
